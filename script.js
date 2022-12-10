e4fb44f6800b662bcc5877587c2dccfb970a2095
denilsonslima <denilsondslima@gmail.com>
2022-12-10 15:42:07 -0400

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
-const input = document.querySelector("#name");
+let input = document.querySelector("#name");
 const btn = document.querySelector("#formulario");
-let elementosClicados, url;
+const api = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
+const apiPost = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
+let elementosClicados;
 let value = ""
 let pegarValue;
 let modeloEscolhido, golaEscolhido, tecidoEscolhido, novoPedido;
+let nameUser = prompt("Informe seu nome: ")
 
-// let nameUser = prompt("Informe seu nome: ")
-// while(nameUser.length <  3){
-//     nameUser = prompt("Informe seu nome: ")
-// }
-let nameUser = "Eu"
-
-function selecionarItem(seletor){
+function selecionarItem(seletor) {
     const pai = seletor.querySelector(".fotos");
     const clicado = seletor.parentNode.parentNode.querySelector(".clicado");
-    if (clicado !== null){
+    if (clicado !== null) {
         clicado.classList.remove("clicado")
     }
     pai.classList.add("clicado")
     elementosClicados = document.querySelectorAll(".clicado")
     const secao = seletor.parentNode.parentNode.classList.value;
-    if(secao === "secao-modelo"){
+    if (secao === "secao-modelo") {
         modeloEscolhido = seletor.classList.value;
         botaoClicavel()
-    } else if (secao === "secao-gola"){
+    } else if (secao === "secao-gola") {
         golaEscolhido = seletor.classList.value;
         botaoClicavel()
-    } else {  
-        tecidoEscolhido = seletor.classList.value; 
+    } else {
+        tecidoEscolhido = seletor.classList.value;
         botaoClicavel()
     }
 }
 
-function botaoClicavel(){
-    const botao = document.querySelector(".button")
-    if ((url !== undefined) && (url !== "")){
-        if(elementosClicados.length === 3){
-            botao.style.background = "#404EED"
-            pegarValue = true
-        }
-    } else {
+const botao = document.querySelector(".button")
+function botaoClicavel() {
+    if (elementosClicados.length === 3 && value !== "") {
+        botao.style.background = "#404EED"
+        pegarValue = true
+    } 
+    
+    else {
         botao.style.background = "#C4C4C4";
         pegarValue = false
     }
 }
 
-function trim(str) {
-    return str
-}
-
-input.onkeyup = function(){ 
-    url = value = trim(input.value)
+input.onkeyup = () => {
+    value = input.value
     botaoClicavel()
 }
 
-btn.addEventListener("submit", function(e){
-    e.preventDefault(); 
-    criarPedido() 
-    if(pegarValue){
-        const name = document.getElementById("name");
-        name.value = ""
+btn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (pegarValue) {
+        document.querySelector(".secao-modelo .clicado").classList.remove("clicado")
+        document.querySelector(".secao-gola .clicado").classList.remove("clicado")
+        document.querySelector(".secao-tecido .clicado").classList.remove("clicado")
+        botao.style.background = "#C4C4C4";
+        pegarValue = false
+        input.value = ""
+        criarPedido()
     }
 })
 
 
-function criarPedido (){
-    novoPedido= 
+
+function criarPedido() {
+    novoPedido =
     {
         model: modeloEscolhido,
         neck: golaEscolhido,
@@ -74,42 +72,35 @@ function criarPedido (){
         owner: nameUser,
         author: "Denilson Lima"
     }
-    console.log(novoPedido)
-} 
 
-//     const requisicao = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", novoPedido);
-//     requisicao.then(deuCerto)
-//     requisicao.catch(function (){})
-//     function deuCerto(){
-//         carregarPedidos()
-//     }
-// }
+    axios.post(apiPost, novoPedido)
+        .then(carregarPedidos)
+        .catch(console.log("erro"))
+}
 
-// const api = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
-// function carregarPedidos(){
-//     axios.get(api)
-//     .then(ana)
-//     .catch(error)
-// } 
 
-// function ana(promessa){
-//     const pedido = promessa.data
-//     const camisas = document.querySelector(".camisas")
-//     camisas.innerHTML = ""
-//     for(let i = 0; i < pedido.length; i++){
-//         camisas.innerHTML+= `
-//         <div class="caixa">
-//             <div class="foto"></div>
-//             <h2>Criador: ${pedido[i].owner}</h2>
-//         </div>
-//         `
-//     }
-//     const fotos = document.querySelectorAll(".foto") 
-//     for(let i = 0; i < fotos.length; i++){
-//         fotos[i].style.backgroundImage = `url(${pedido[i].image})`
-//     }
-// } 
+function carregarPedidos() {
+    axios.get(api)
+        .then(renderizarPedidos)
+        .catch(console.log("erro"))
+}
 
-// function error(erro){
-//     console.log(erro)
-// } carregarPedidos()
+function renderizarPedidos(p) {
+    const pedido = p.data
+    const camisas = document.querySelector(".camisas")
+    camisas.innerHTML = ""
+    for (let i = 0; i < pedido.length; i++) {
+        camisas.innerHTML += `
+            <div class="caixa">
+                <div class="foto">
+                    <img src="${pedido[i].image}" alt="imagem blusa"/>
+                </div>
+                <h2>Criador: ${pedido[i].owner}</h2>
+            </div>
+        `
+    }
+}
+
+
+
+carregarPedidos()
